83b22d33aa0aa365edb4b1f54527479dd0662475
Nicolas <78997194+nicolaspiet@users.noreply.github.com>
2023-04-16 14:48:28 -0300

    fixed - keep writing the same words.

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ delays = [0.07, 0.03, 0.08, 0.07, 0.5]
 long_words = True
 instant_typing = True
 pyautogui.PAUSE = 0
+used_words = []
 
 with open('wordlist.txt') as word_file:
     valid_words = set(word_file.read().split())
 
-
 def release(key):
-    global bomb_x, bomb_y, length
+    global bomb_x, bomb_y, length, used_words
     if key == Key.f8:
         try:
             bomb_x, bomb_y = pyautogui.position()
@@ -54,6 +54,16 @@ def release(key):
             else:
                 choice = random.randint(0, arrsize - 1)
             final_word = found_words[choice].strip('"').strip(':').strip('"')
+
+            while final_word in used_words:
+                if len(used_words) == arrsize:
+                    print("All words have been used.")
+                    return True
+                choice = random.randint(0, arrsize - 1)
+                final_word = found_words[choice].strip('"').strip(':').strip('"')
+
+            used_words.append(final_word)
+
             if instant_typing:
                 pyperclip.copy(f'{final_word}')
                 with pyautogui.hold('ctrl'):
@@ -64,15 +74,13 @@ def release(key):
                     pyautogui.write(w, delay)
             time.sleep(0.1)
             pyautogui.press('enter')
-            print(found_words[choice])
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
-            
+
             return True
 
 
 with Listener(on_release=release) as listener:
     listener.join()
-
